04b3197febfc545d0a08224f9aaf9fb933a1e256
morenourenam <morenourenam@gmail.com>
2026-09-12 17:01:50 +0000

    ex

--- introduction/X54725.cc
+++ introduction/X54725.cc
@@ -1,1 +1,14 @@
 //Minimum of two integers
+#include <iostream>
+ using namespace std;
+
+ int main(){
+    int x, y;
+    cin >> x >> y;
+    if (x > y){
+        cout << y << endl;
+    }
+    else{
+    cout << x << endl;
+    }
+ }
--- introduction/X71570.cc
+++ introduction/X71570.cc
@@ -1,1 +1,10 @@
  //Difference of two integers
+ #include <iostream>
+ using namespace std;
+
+ int main(){
+    int x, y;
+    cin >> x >> y;
+    int s = x-y;
+    cout << s << endl;
+ }
